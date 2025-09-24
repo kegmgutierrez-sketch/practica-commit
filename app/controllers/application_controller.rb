c609d011d894185421de407c41ca01e91e4e4992
kevin-gutierrez <kegmgutierrez@gmail.com>
2025-09-23 20:05:15 -0600

    Agregada acción 'saludo' en ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-def hola
-    render html: "hola, mundo!"
-  end
+#def hola
+ # render html: "hola, mundo!"
+ # end
+def saludo
+  render html: "Bienvenido a mi app Rails!"
+end
+
+
 end
